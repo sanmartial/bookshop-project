4d343e69cdf9efccb836c4a5246adac381228bcd
Roman Globa <globaroman@gmail.com>
2023-12-11 19:55:42 +0200

    Created project bookshop

---
+++ src/main/java/com/globaroman/bookshopproject/BookshopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.globaroman.bookshopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globaroman/bookshopproject/BookshopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaroman.bookshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
