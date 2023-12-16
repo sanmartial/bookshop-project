9df08f613549f1c01a5b108e7d2b186d17889e0c
Roman Globa <globaroman@gmail.com>
2023-12-16 02:11:27 +0200

    Implemented JpaRepository, added delete and update metods, added Liquibase support

--- src/main/java/com/globaroman/bookshopproject/mapper/BookMapper.java
+++ src/main/java/com/globaroman/bookshopproject/mapper/BookMapper.java
@@ -5,10 +5,16 @@ import com.globaroman.bookshopproject.dto.BookDto;
 import com.globaroman.bookshopproject.dto.CreateBookRequestDto;
 import com.globaroman.bookshopproject.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
+
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateBookFromDto(@MappingTarget Book book, CreateBookRequestDto bookRequestDto);
 }
--- src/main/java/com/globaroman/bookshopproject/model/Book.java
+++ src/main/java/com/globaroman/bookshopproject/model/Book.java
@@ -11,9 +11,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "books")
+@SQLDelete(sql = "UPDATE books SET  is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
 @Data
 @Builder
 @AllArgsConstructor
@@ -32,5 +36,7 @@ public class Book {
     @Column(name = "price", nullable = false)
     private BigDecimal price;
     private String description;
+    @Column(name = "cover_image")
     private String coverImage;
+    private boolean isDeleted = false;
 }
