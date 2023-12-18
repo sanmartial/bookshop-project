6ab148239cf540c95e08b12ee5aafcd077e922ac
Roman Globa <globaroman@gmail.com>
2023-12-18 22:31:05 +0200

    Added CustomGlobalExceptionHandler class and validation for CreateBookRequestDto

--- src/main/java/com/globaroman/bookshopproject/dto/CreateBookRequestDto.java
+++ src/main/java/com/globaroman/bookshopproject/dto/CreateBookRequestDto.java
@@ -1,5 +1,8 @@
 package com.globaroman.bookshopproject.dto;
 
+import com.globaroman.bookshopproject.validation.Isbn;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Builder;
 import lombok.Data;
@@ -7,10 +10,18 @@ import lombok.Data;
 @Data
 @Builder
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @Isbn
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
---
+++ src/main/java/com/globaroman/bookshopproject/validation/IsbnValidator.java
@@ -0,0 +1,14 @@
+package com.globaroman.bookshopproject.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+
+public class IsbnValidator implements ConstraintValidator<Isbn, String> {
+    private final String patternOfIsbn = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d -]{13}$";
+
+    @Override
+    public boolean isValid(String isbn, ConstraintValidatorContext constraintValidatorContext) {
+        return isbn != null && Pattern.compile(patternOfIsbn).matcher(isbn).matches();
+    }
+}
