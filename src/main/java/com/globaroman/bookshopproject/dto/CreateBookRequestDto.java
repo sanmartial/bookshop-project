b439608c74ce9ac2521008b6210fc23b15c667cd
Roman Globa <globaroman@gmail.com>
2023-12-19 21:18:36 +0200

    CheckStyle+

--- src/main/java/com/globaroman/bookshopproject/dto/CreateBookRequestDto.java
+++ src/main/java/com/globaroman/bookshopproject/dto/CreateBookRequestDto.java
@@ -1,16 +1,12 @@
 package com.globaroman.bookshopproject.dto;
 
 import com.globaroman.bookshopproject.validation.Isbn;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
-
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import java.math.BigDecimal;
 
 @Data
 @Builder
