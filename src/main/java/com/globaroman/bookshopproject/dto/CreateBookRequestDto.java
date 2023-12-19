2a9aeedbf54a081dd5c2767d7e48cb3b091a98b5
Roman Globa <globaroman@gmail.com>
2023-12-19 21:17:11 +0200

    reduced number of annotations

--- src/main/java/com/globaroman/bookshopproject/dto/CreateBookRequestDto.java
+++ src/main/java/com/globaroman/bookshopproject/dto/CreateBookRequestDto.java
@@ -3,8 +3,11 @@ package com.globaroman.bookshopproject.dto;
 import com.globaroman.bookshopproject.validation.Isbn;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -12,25 +15,19 @@ import org.hibernate.validator.constraints.Length;
 @Data
 @Builder
 public class CreateBookRequestDto {
-    @NotNull
     @NotBlank
     private String title;
-    @NotNull
     @NotBlank
     private String author;
-    @NotNull
     @NotBlank
     @Isbn
     private String isbn;
-    @NotNull
     @NotBlank
-    @Min(0)
+    @PositiveOrZero
     private BigDecimal price;
-    @NotNull
     @NotBlank
-    @Length(min = 0, max = 1055)
+    @Length(max = 1055)
     private String description;
-    @NotNull
     @NotBlank
     private String coverImage;
 }
