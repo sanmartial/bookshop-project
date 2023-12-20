2eee3e4c70121e8877e018c1130e9724d61d8f9a
Roman Globa <globaroman@gmail.com>
2023-12-20 16:13:20 +0200

    Added Pagination, Sorting, and Swagger to the Bookcontroller

--- src/main/java/com/globaroman/bookshopproject/service/impl/BookServiceImpl.java
+++ src/main/java/com/globaroman/bookshopproject/service/impl/BookServiceImpl.java
@@ -11,6 +11,7 @@ import com.globaroman.bookshopproject.repository.specification.book.BookSpecific
 import com.globaroman.bookshopproject.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -29,9 +30,9 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
+    public List<BookDto> findAll(Pageable pageable) {
 
-        return bookRepository.findAll()
+        return bookRepository.findAll(pageable)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();
