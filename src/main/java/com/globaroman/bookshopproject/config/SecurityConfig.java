1d980c7ac1e851277134faf55e3302ed7175d79e
Roman Globa <globaroman@gmail.com>
2023-12-29 00:28:32 +0200

    Implemented basic security to project

---
+++ src/main/java/com/globaroman/bookshopproject/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.globaroman.bookshopproject.config;
+
+import com.globaroman.bookshopproject.security.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity()
+public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoderConfig passwordEncoderConfig;
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoderConfig.passwordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/api/auth/**", "/swagger-ui/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsService)
+                .build();
+    }
+}
--- src/main/java/com/globaroman/bookshopproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/globaroman/bookshopproject/service/impl/UserServiceImpl.java
@@ -1,30 +1,54 @@
 package com.globaroman.bookshopproject.service.impl;
 
+import com.globaroman.bookshopproject.config.PasswordEncoderConfig;
 import com.globaroman.bookshopproject.dto.UserRegistrationRequestDto;
 import com.globaroman.bookshopproject.dto.UserResponseDto;
 import com.globaroman.bookshopproject.exception.RegistrationException;
 import com.globaroman.bookshopproject.mapper.UserMapper;
+import com.globaroman.bookshopproject.model.Role;
+import com.globaroman.bookshopproject.model.User;
+import com.globaroman.bookshopproject.repository.RoleRepository;
 import com.globaroman.bookshopproject.repository.UserRepository;
 import com.globaroman.bookshopproject.service.UserService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private static final Long ROLE_ID_IN_DB =
+            (long) Role.RoleName.USER.ordinal() + 1;
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoderConfig passwordEncoderConfig;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
-
             throw new RegistrationException(
                     "Can't register user. The same user with email "
                             + requestDto.getEmail()
                             + " already exist");
         }
-        return userMapper.toDto(userRepository.save(userMapper.toModel(requestDto)));
+
+        User user = getUserWithRoleAndPasswordEncode(requestDto);
+        return userMapper.toDto(userRepository.save(user));
+    }
+
+    private User getUserWithRoleAndPasswordEncode(UserRegistrationRequestDto requestDto) {
+        User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoderConfig
+                .passwordEncoder().encode(requestDto.getPassword()));
+        user.setRoles(Set.of(getRoleFromDB(ROLE_ID_IN_DB)));
+        return user;
+    }
+
+    private Role getRoleFromDB(Long id) {
+        return roleRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("Can't find role with id: " + id));
     }
 }
