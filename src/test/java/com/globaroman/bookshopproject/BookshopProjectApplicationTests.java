d104278cda90d826046af80eddfb57052f1e246d
Roman Globa <globaroman@gmail.com>
2024-01-02 13:32:59 +0200

    Uncommented annotation @Test

--- src/test/java/com/globaroman/bookshopproject/BookshopProjectApplicationTests.java
+++ src/test/java/com/globaroman/bookshopproject/BookshopProjectApplicationTests.java
@@ -1,11 +1,12 @@
 package com.globaroman.bookshopproject;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookshopProjectApplicationTests {
 
-    //@Test
+    @Test
     void contextLoads() {
     }
 
